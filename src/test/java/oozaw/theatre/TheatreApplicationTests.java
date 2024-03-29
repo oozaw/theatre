b240bd769ed15a9720f6c6b30de5d01a7ab3c39b
oozaw <79121990+oozaw@users.noreply.github.com>
2024-03-29 13:14:01 +0700

    initial commit

---
+++ src/test/java/oozaw/theatre/TheatreApplicationTests.java
@@ -0,0 +1,13 @@
+package oozaw.theatre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheatreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
