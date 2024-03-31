41490dc0dbee2a13a73aab3e03ef18ae83689459
oozaw <79121990+oozaw@users.noreply.github.com>
2024-03-31 12:58:38 +0700

    update user response class

--- src/main/java/oozaw/theatre/model/UserResponse.java
+++ src/main/java/oozaw/theatre/model/UserResponse.java
@@ -4,15 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import oozaw.theatre.entity.User;
 
 import java.sql.Time;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class UserResponse {
-    
+
     private String id;
 
     private String name;
@@ -21,7 +23,18 @@ public class UserResponse {
 
     private String phone;
 
-    private Time createdAt;
+    private LocalDateTime createdAt;
 
-    private Time updatedAt;
+    private LocalDateTime updatedAt;
+
+    public static UserResponse fromEntity(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .phone(user.getPhone())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
+                .build();
+    }
 }
