7ace51b51a0ffa965fecb9147f5ddef058993ec4
oozaw <79121990+oozaw@users.noreply.github.com>
2024-04-02 07:48:16 +0700

    add resolver for handle api token request

---
+++ src/main/java/oozaw/theatre/resolver/UserArgumentResolver.java
@@ -0,0 +1,53 @@
+package oozaw.theatre.resolver;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import oozaw.theatre.entity.User;
+import oozaw.theatre.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.server.ResponseStatusException;
+
+@Component
+@Slf4j
+public class UserArgumentResolver implements HandlerMethodArgumentResolver {
+
+   @Autowired
+   private UserRepository userRepository;
+
+   @Override
+   public boolean supportsParameter(MethodParameter parameter) {
+      return parameter.getParameterType().equals(User.class);
+   }
+
+   @Override
+   public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+      HttpServletRequest servletRequest = (HttpServletRequest) webRequest.getNativeRequest();
+      String token = servletRequest.getHeader("X-API-KEY");
+
+      log.info("TOKEN: {}", token);
+
+      if (token == null) {
+         throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized");
+      }
+
+      User user = userRepository.findFirstByToken(token).orElseThrow(
+         () -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized")
+      );
+
+      log.info("USER: {}", user);
+
+      if (user.getTokenExpiredAt() < System.currentTimeMillis()) {
+         throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized");
+      }
+
+      return user;
+   }
+}
